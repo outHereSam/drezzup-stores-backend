811df725df0339bf80ad020737616e6e25544191
Samuel Larbi Danquah <larbisamuel0@gmail.com>
2025-01-09 12:01:43 +0000

    Adds refresh token route

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -3,6 +3,7 @@ import {
   registerUser,
   loginUser,
   logoutUser,
+  refreshToken,
 } from "../controllers/authController";
 
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.post("/logout", logoutUser);
+router.post("/refresh", refreshToken);
 
 export default router;
